16c6492d7c4f5a1480913d74c83b13de00ccb77c
aladdin4u <taofikakinkumi@gmail.com>
2023-10-17 10:17:58 +0100

    report CRUD operation

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
-const { GraphQlError, GraphQLScalarType } = require("graphql");
+const { GraphQLError, GraphQLScalarType } = require("graphql");
 const { Kind } = require("graphql/language");
 
 let reports = [
@@ -49,14 +49,16 @@ const typeDefs = `
         name: String!
         date: Date!
         bodymap: String! 
-        details: String! 
-        id: ID! 
+        details: String!
       ): Report
       editReport(
+        id: ID!
         name: String!
         date: Date!
         bodymap: String!
-        details: String! 
+        details: String!
+      ): Report
+      deleteReport( 
         id: ID! 
       ): Report
     }
@@ -67,6 +69,48 @@ const resolvers = {
     ReportCount: () => reports.length,
     allreports: () => reports,
     findReport: (root, args) => reports.find((r) => r.name === args.name),
+  },
+  Mutation: {
+    addReport: (root, args) => {
+      if (reports.find((r) => r.id == args.id)) {
+        throw new GraphQLError("Id must be unique", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.id,
+          },
+        });
+      }
+
+      const report = { ...args, id: reports.length + 1 };
+      reports = reports.concat(report);
+      console.log(report);
+      return report;
+    },
+    editReport: (root, args) => {
+      const report = reports.find((r) => r.id == args.id);
+      if (!report) {
+        return null;
+      }
+
+      const editedReport = {
+        ...report,
+        name: args.name,
+        date: args.date,
+        bodymap: args.bodymap,
+        details: args.details,
+      };
+      reports = reports.map((r) => (r.id == args.id ? editedReport : r));
+      return editedReport;
+    },
+    deleteReport: (root, args) => {
+      const report = reports.find((r) => r.id == args.id);
+      if (!report) {
+        return null;
+      }
+
+      reports = reports.filter((r) => r.id != report.id);
+      return report;
+    },
   },
   Date: new GraphQLScalarType({
     name: "Date",
